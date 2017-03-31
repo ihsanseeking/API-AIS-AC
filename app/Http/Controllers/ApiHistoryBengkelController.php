89f976e386e61540845abd13f93c3f848c77b261
sanseeking <ihsan.seeking@gmail.com>
2017-03-31 13:32:18 +0700

    fixed history

--- app/Http/Controllers/ApiHistoryBengkelController.php
+++ app/Http/Controllers/ApiHistoryBengkelController.php
@@ -33,7 +33,6 @@
 				$list_bengkel = $postdata['list_bengkel_id'];
 				$list_customer = $postdata['list_customer_id'];
 				
-				
 				$historys = DB::table('order')
 				->join('customer', 'customer.id', '=', 'order.customer_id')
 				->leftjoin('vehicle', 'vehicle.user_id', '=', 'order.customer_id')
@@ -43,345 +42,271 @@
 				->join('service', 'service.id', '=', 'order.ref_service_id')
 				->join('bengkel', 'bengkel.id', '=', 'service.bengkel_id')
 				->join('ref_service_type', 'ref_service_type.id', '=', 'service.ref_service_id');
-				
-				$historys_all = $historys->select(
-					'order.id', 
-					'order.order_date', 
-					'order.order_time', 
-					'order.service_date', 
-					'order.service_time',
-					'order.service_location', 
-					'order.latitude',
-					'order.longitude',
-					'order.area_id',
-					'order.is_emergency',
-					'order.status',
-					'order.method',
-					'order.payment_status',
-					
-					'order.customer_id',
-					'customer.name',
-					'customer.email',
-					'customer.cellphone',
-					'customer.address',
-					'customer.deviceid',
-					'customer.ref_occupation_id',
-					
-					'vehicle.user_id',
-					'vehicle.year',
-					
-					'vehicle.ref_vehicle_type_id',
-					'ref_vehicle_type.wheel',
-					'ref_vehicle_type.type',
-					'ref_vehicle_type.transmission',
-					
-					'ref_vehicle_type.ref_brand_id',
-					'ref_brand.name',
-					
-					'review.id',
-					'review.penilaian',
-					'review.feedback',
-					
-					'order.ref_service_id',
-					'service.hb',
-					'service.hj',
-					'service.jp',
-					'service.bd',
-					
-					'service.ref_service_id',
-					'ref_service_type.sub as service_sub',
-					'ref_service_type.name as service_name',
-					'ref_service_type.wheel',
-					'ref_service_type.description',
-					
-					'service.bengkel_id',
-					'bengkel.name',
-					'bengkel.address',
-					'bengkel.latitude',
-					'bengkel.longitude',
-					'bengkel.ref_area_id',
-					'bengkel.phone',
-					'bengkel.fax',
-					'bengkel.email',
-					'bengkel.account_name',
-					'bengkel.account_number',
-					
-					'ref_vehicle_type.type as vehicle_name'
-				);
-				
-				$history_detail_bengkel = $historys->select(
-					'order.id', 
-					'order.order_date', 
-					'order.order_time', 
-					'order.service_date', 
-					'order.service_time',
-					'order.service_location', 
-					'order.latitude',
-					'order.longitude',
-					//'order.area_id',
-					//'order.is_emergency',
-					'order.status',
-					'order.method',
-					'order.payment_status',
-					
-					'order.customer_id',
-					'customer.name',
-					//'customer.email',
-					//'customer.cellphone',
-					//'customer.address',
-					//'customer.deviceid',
-					//'customer.ref_occupation_id',
-					
-					//'vehicle.user_id',
-					'vehicle.year',
-					
-					//'vehicle.ref_vehicle_type_id',
-					'ref_vehicle_type.wheel',
-					'ref_vehicle_type.type',
-					//'ref_vehicle_type.transmission',
-					
-					//'ref_vehicle_type.ref_brand_id',
-					'ref_brand.name',
-					
-					//'review.id',
-					'review.penilaian',
-					'review.feedback',
-					
-					//'order.ref_service_id',
-					//'service.hb',
-					'service.hj',
-					'service.jp',
-					'service.bd',
-					//'total.?',
-					
-					//'service.ref_service_id',
-					'ref_service_type.sub as service_sub',
-					'ref_service_type.name as service_name',
-					//'ref_service_type.wheel',
-					//'ref_service_type.description',
-					
-					//'service.bengkel_id',
-					'bengkel.name',
-					'bengkel.address',
-					'bengkel.latitude',
-					'bengkel.longitude',
-					'bengkel.ref_area_id'
-					//'bengkel.phone',
-					//'bengkel.fax',
-					//'bengkel.email',
-					//'bengkel.account_name',
-					//'bengkel.account_number'
-				);
-				
-				$history_list_bengkel = $historys->select(
-					'order.id', 
-					'order.order_date', 
-					'order.order_time', 
-					'order.service_date', 
-					'order.service_time',
-					'order.service_location', 
-					'order.latitude',
-					'order.longitude',
-					//'order.area_id',
-					//'order.is_emergency',
-					//'order.status',
-					//'order.method',
-					//'order.payment_status',
-					
-					'order.customer_id',
-					//'customer.name',
-					//'customer.email',
-					//'customer.cellphone',
-					//'customer.address',
-					//'customer.deviceid',
-					//'customer.ref_occupation_id',
-					
-					//'vehicle.user_id',
-					'vehicle.year',
-					
-					//'vehicle.ref_vehicle_type_id',
-					'ref_vehicle_type.wheel',
-					'ref_vehicle_type.type',
-					//'ref_vehicle_type.transmission',
-					
-					//'ref_vehicle_type.ref_brand_id',
-					'ref_brand.name',
-					
-					//'review.id',
-					'review.penilaian',
-					'review.feedback',
-					
-					//'order.ref_service_id',
-					//'service.hb',
-					//'service.hj',
-					//'service.jp',
-					//'service.bd',
-					//'total.?',
-					
-					//'service.ref_service_id',
-					'ref_service_type.sub as service_sub',
-					'ref_service_type.name as service_name',
-					//'ref_service_type.wheel',
-					//'ref_service_type.description',
-					
-					'service.bengkel_id',
-					//'bengkel.name',
-					//'bengkel.address',
-					'bengkel.latitude',
-					'bengkel.longitude'
-					//'bengkel.ref_area_id',
-					//'bengkel.phone',
-					//'bengkel.fax',
-					//'bengkel.email',
-					//'bengkel.account_name',
-					//'bengkel.account_number
-				);
-				
-				$history_detail_customer = $historys->select(
-					'order.id', 
-					'order.order_date', 
-					'order.order_time', 
-					'order.service_date', 
-					'order.service_time',
-					'order.service_location', 
-					'order.latitude',
-					'order.longitude',
-					//'order.area_id',
-					//'order.is_emergency',
-					'order.status',
-					'order.method',
-					'order.payment_status',
-					
-					'order.customer_id',
-					//'customer.name',
-					//'customer.email',
-					//'customer.cellphone',
-					//'customer.address',
-					//'customer.deviceid',
-					//'customer.ref_occupation_id',
-					
-					//'vehicle.user_id',
-					'vehicle.year',
-					
-					//'vehicle.ref_vehicle_type_id',
-					'ref_vehicle_type.wheel',
-					'ref_vehicle_type.type',
-					//'ref_vehicle_type.transmission',
-					
-					//'ref_vehicle_type.ref_brand_id',
-					'ref_brand.name',
-					
-					//'review.id',
-					//'review.penilaian',
-					//'review.feedback',
-					
-					//'order.ref_service_id',
-					//'service.hb',
-					'service.hj',
-					'service.jp',
-					'service.bd',
-					//'total.?',
-					
-					//'service.ref_service_id',
-					'ref_service_type.sub as service_sub',
-					'ref_service_type.name as service_name',
-					//'ref_service_type.wheel',
-					//'ref_service_type.description',
-					
-					'service.bengkel_id',
-					'bengkel.name',
-					'bengkel.address',
-					'bengkel.latitude',
-					'bengkel.longitude'
-					//'bengkel.ref_area_id',
-					//'bengkel.phone',
-					//'bengkel.fax',
-					//'bengkel.email',
-					//'bengkel.account_name',
-					//'bengkel.account_number'
-				);
-				
-				$history_list_customer = $historys->select(
-					'order.id', 
-					'order.order_date', 
-					'order.order_time', 
-					'order.service_date', 
-					'order.service_time',
-					'order.service_location', 
-					'order.latitude',
-					'order.longitude',
-					//'order.area_id',
-					//'order.is_emergency',
-					//'order.status',
-					//'order.method',
-					//'order.payment_status',
-					
-					'order.customer_id',
-					//'customer.name',
-					//'customer.email',
-					//'customer.cellphone',
-					//'customer.address',
-					//'customer.deviceid',
-					//'customer.ref_occupation_id',
-					
-					//'vehicle.user_id',
-					'vehicle.year',
-					
-					//'vehicle.ref_vehicle_type_id',
-					'ref_vehicle_type.wheel',
-					'ref_vehicle_type.type',
-					//'ref_vehicle_type.transmission',
-					
-					//'ref_vehicle_type.ref_brand_id',
-					'ref_brand.name',
-					
-					//'review.id',
-					//'review.penilaian',
-					//'review.feedback',
-					
-					//'order.ref_service_id',
-					//'service.hb',
-					//'service.hj',
-					//'service.jp',
-					//'service.bd',
-					//'total.?',
-					
-					//'service.ref_service_id',
-					'ref_service_type.sub as service_sub',
-					'ref_service_type.name as service_name',
-					//'ref_service_type.wheel',
-					//'ref_service_type.description',
-					
-					'service.bengkel_id',
-					//'bengkel.name',
-					//'bengkel.address',
-					'bengkel.latitude',
-					'bengkel.longitude'
-					//'bengkel.ref_area_id',
-					//'bengkel.phone',
-					//'bengkel.fax',
-					//'bengkel.email',
-					//'bengkel.account_name',
-					//'bengkel.account_number'
-				);
-				
+							
 				if (!is_null($detail_bengkel)){
-					 
+					$history_detail_bengkel = $historys->select(
+						'order.id as order_id', 
+						'order.order_date as order_date', 
+						'order.order_time as order_time', 
+						'order.service_date as order_service_date', 
+						'order.service_time as order_service_time',
+						'order.service_location as order_service_location', 
+						'order.latitude as order_latitude',
+						'order.longitude as order_logitude',
+						//'order.area_id as order_area',
+						//'order.is_emergency as order_emergency',
+						'order.status as order_status',
+						'order.method as order_method',
+						'order.payment_status as order_payment_status',
+						
+						'order.customer_id as order_customer_id',
+						'customer.name as customer_name',
+						//'customer.email as customer_email',
+						//'customer.cellphone as customer_cellphone',
+						//'customer.address as customer_address',
+						//'customer.deviceid as customer_device',
+						//'customer.ref_occupation_id as customer_occupation',
+						
+						//'vehicle.user_id as vehicle_customer_id',
+						'vehicle.year as vehicle_year',
+						
+						//'vehicle.ref_vehicle_type_id as vehicle_type_id',
+						'ref_vehicle_type.wheel as vehicle_wheel',
+						'ref_vehicle_type.type as vehicle_type',
+						'ref_vehicle_type.transmission as vehicle_transmision',
+						
+						//'ref_vehicle_type.ref_brand_id as vehicle_brand_id',
+						'ref_brand.name as brand_name',
+						
+						'review.id as review_id',
+						'review.penilaian as review_penilaian',
+						'review.feedback as review_feedback',
+						
+						//'order.ref_service_id as order_service_id',
+						'service.hb as servic_hb',
+						'service.hj as service_hj',
+						'service.jp as service_jp',
+						'service.bd as service_bd',
+						//'total.?',
+						
+						//'service.ref_service_id as service_type_id',
+						'ref_service_type.sub as service_sub',
+						'ref_service_type.name as service_name',
+						'ref_service_type.wheel as service_wheel',
+						//'ref_service_type.description as service_desc',
+						
+						//'service.bengkel_id as service_bengkel_id',
+						//'bengkel.name as bengkel_name',
+						//'bengkel.address as bengkel_address',
+						'bengkel.latitude as bengkel_latitude',
+						'bengkel.longitude as bengkel_longitude'
+						//'bengkel.ref_area_id as bengkel_area',
+						//'bengkel.phone as bengkel_phone',
+						//'bengkel.fax as bengkel_fax',
+						//'bengkel.email as bengkel_email',
+						//'bengkel.account_name as bengkel_account_name',
+						//'bengkel.account_number as bengkel_account_num '
+					); 
 					$history = $history_detail_bengkel->where('order.id', '=', $detail_bengkel)->first();
 				} elseif (!is_null($detail_customer)){
-					
+					$history_detail_customer = $historys->select(
+						'order.id as order_id', 
+						'order.order_date as order_date', 
+						'order.order_time as order_time', 
+						'order.service_date as order_service_date', 
+						'order.service_time as order_service_time',
+						'order.service_location as order_service_location', 
+						'order.latitude as order_latitude',
+						'order.longitude as order_logitude',
+						'order.area_id as order_area',
+						//'order.is_emergency as order_emergency',
+						'order.status as order_status',
+						'order.method as order_method',
+						'order.payment_status as order_payment_status',
+						
+						//'order.customer_id as order_customer_id',
+						//'customer.name as customer_name',
+						//'customer.email as customer_email',
+						//'customer.cellphone as customer_cellphone',
+						//'customer.address as customer_address',
+						//'customer.deviceid as customer_device',
+						//'customer.ref_occupation_id as customer_occupation',
+						
+						//'vehicle.user_id as vehicle_customer_id',
+						'vehicle.year as vehicle_year',
+						
+						//'vehicle.ref_vehicle_type_id as vehicle_type_id',
+						'ref_vehicle_type.wheel as vehicle_wheel',
+						'ref_vehicle_type.type as vehicle_type',
+						'ref_vehicle_type.transmission as vehicle_transmision',
+						
+						//'ref_vehicle_type.ref_brand_id as vehicle_brand_id',
+						'ref_brand.name as brand_name',
+						
+						//'review.id as review_id',
+						//'review.penilaian as review_penilaian',
+						//'review.feedback as review_feedback',
+						
+						//'order.ref_service_id as order_service_id',
+						'service.hb as servic_hb',
+						'service.hj as service_hj',
+						'service.jp as service_jp',
+						'service.bd as service_bd',
+						//'total.?',
+						
+						//'service.ref_service_id as service_type_id',
+						'ref_service_type.sub as service_sub',
+						'ref_service_type.name as service_name',
+						'ref_service_type.wheel as service_wheel',
+						//'ref_service_type.description as service_desc',
+						
+						//'service.bengkel_id as service_bengkel_id',
+						'bengkel.name as bengkel_name',
+						'bengkel.address as bengkel_address',
+						'bengkel.latitude as bengkel_latitude',
+						'bengkel.longitude as bengkel_longitude',
+						'bengkel.ref_area_id as bengkel_area',
+						'bengkel.phone as bengkel_phone',
+						'bengkel.fax as bengkel_fax',
+						'bengkel.email as bengkel_email',
+						'bengkel.account_name as bengkel_account_name',
+						'bengkel.account_number as bengkel_account_num '
+					);
 					$history = $history_detail_customer->where('order.id', '=', $detail_customer)->first();
 				} elseif (!is_null($list_bengkel)){
-					
+					$history_list_bengkel = $historys->select(
+						'order.id as order_id', 
+						'order.order_date as order_date', 
+						'order.order_time as order_time', 
+						'order.service_date as order_service_date', 
+						'order.service_time as order_service_time',
+						'order.service_location as order_service_location', 
+						'order.latitude as order_latitude',
+						'order.longitude as order_logitude',
+						//'order.area_id as order_area',
+						//'order.is_emergency as order_emergency',
+						//'order.status as order_status',
+						//'order.method as order_method',
+						//'order.payment_status as order_payment_status',
+						
+						//'order.customer_id as order_customer_id',
+						//'customer.name as customer_name',
+						//'customer.email as customer_email',
+						//'customer.cellphone as customer_cellphone',
+						//'customer.address as customer_address',
+						//'customer.deviceid as customer_device',
+						//'customer.ref_occupation_id as customer_occupation',
+						
+						//'vehicle.user_id as vehicle_customer_id',
+						'vehicle.year as vehicle_year',
+						
+						//'vehicle.ref_vehicle_type_id as vehicle_type_id',
+						'ref_vehicle_type.wheel as vehicle_wheel',
+						'ref_vehicle_type.type as vehicle_type',
+						'ref_vehicle_type.transmission as vehicle_transmision',
+						
+						//'ref_vehicle_type.ref_brand_id as vehicle_brand_id',
+						'ref_brand.name as brand_name',
+						
+						//'review.id as review_id',
+						'review.penilaian as review_penilaian',
+						//'review.feedback as review_feedback',
+						
+						//'order.ref_service_id as order_service_id',
+						//'service.hb as servic_hb',
+						//'service.hj as service_hj',
+						//'service.jp as service_jp',
+						//'service.bd as service_bd',
+						//'total.?',
+						
+						//'service.ref_service_id as service_type_id',
+						'ref_service_type.sub as service_sub',
+						'ref_service_type.name as service_name',
+						//'ref_service_type.wheel as service_wheel',
+						//'ref_service_type.description as service_desc',
+						
+						//'service.bengkel_id as service_bengkel_id',
+						//'bengkel.name as bengkel_name',
+						//'bengkel.address as bengkel_address',
+						'bengkel.latitude as bengkel_latitude',
+						'bengkel.longitude as bengkel_longitude'
+						//'bengkel.ref_area_id as bengkel_area',
+						//'bengkel.phone as bengkel_phone',
+						//'bengkel.fax as bengkel_fax',
+						//'bengkel.email as bengkel_email',
+						//'bengkel.account_name as bengkel_account_name',
+						//'bengkel.account_number as bengkel_account_num '
+					); 
 					$history = $history_list_bengkel->where('service.bengkel_id', '=', $list_bengkel)->get();
 				} elseif (!is_null($list_customer)){
-					
+					$history_list_customer = $historys->select(
+						'order.id as order_id', 
+						'order.order_date as order_date', 
+						'order.order_time as order_time', 
+						'order.service_date as order_service_date', 
+						'order.service_time as order_service_time',
+						'order.service_location as order_service_location', 
+						//'order.latitude as order_latitude',
+						//'order.longitude as order_logitude',
+						//'order.area_id as order_area',
+						//'order.is_emergency as order_emergency',
+						//'order.status as order_status',
+						//'order.method as order_method',
+						//'order.payment_status as order_payment_status',
+						
+						'order.customer_id as order_customer_id',
+						//'customer.name as customer_name',
+						//'customer.email as customer_email',
+						//'customer.cellphone as customer_cellphone',
+						//'customer.address as customer_address',
+						//'customer.deviceid as customer_device',
+						//'customer.ref_occupation_id as customer_occupation',
+						
+						//'vehicle.user_id as vehicle_customer_id',
+						'vehicle.year as vehicle_year',
+						
+						//'vehicle.ref_vehicle_type_id as vehicle_type_id',
+						'ref_vehicle_type.wheel as vehicle_wheel',
+						'ref_vehicle_type.type as vehicle_type',
+						'ref_vehicle_type.transmission as vehicle_transmision',
+						
+						//'ref_vehicle_type.ref_brand_id as vehicle_brand_id',
+						'ref_brand.name as brand_name',
+						
+						//'review.id as review_id',
+						//'review.penilaian as review_penilaian',
+						//'review.feedback as review_feedback',
+						
+						//'order.ref_service_id as order_service_id',
+						//'service.hb as servic_hb',
+						//'service.hj as service_hj',
+						//'service.jp as service_jp',
+						//'service.bd as service_bd',
+						//'total.?',
+						
+						//'service.ref_service_id as service_type_id',
+						'ref_service_type.sub as service_sub',
+						'ref_service_type.name as service_name'
+						//'ref_service_type.wheel as service_wheel',
+						//'ref_service_type.description as service_desc',
+						
+						//'service.bengkel_id as service_bengkel_id',
+						//'bengkel.name as bengkel_name',
+						//'bengkel.address as bengkel_address',
+						//'bengkel.latitude as bengkel_latitude',
+						//'bengkel.longitude as bengkel_longitude',
+						//'bengkel.ref_area_id as bengkel_area',
+						//'bengkel.phone as bengkel_phone',
+						//'bengkel.fax as bengkel_fax',
+						//'bengkel.email as bengkel_email',
+						//'bengkel.account_name as bengkel_account_name',
+						//'bengkel.account_number as bengkel_account_num '
+					);
 					$history = $history_list_customer->where('order.customer_id', '=', $list_customer)->get();
 				} else {
 					$history = $historys_all->get();
 				}
 				
-
 				$result['data']=$history;
 				
 				
